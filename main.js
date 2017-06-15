76c6d4a1d9672e3196b632b11620f7dc80e0cfbc
gideon877 <tblar84@gmail.com>
2017-06-15 08:55:58 +0200

    created a filter function

--- main.js
+++ main.js
@@ -1,88 +1,103 @@
 module.exports = function(models) {
 
-        const index = function(req, res, done) {
-            var display = "";
+    const index = function(req, res, done) {
+        var display = "";
 
-            var regData = {
+        var regData = {
+            reg_number: req.body.reg_number
+        }
+
+        if (!regData || !regData.reg_number) {
+            req.flash('error', 'Plate input should not be blank');
+            res.render('reg_numbers');
+        } else {
+
+            models.Plate.find({
                 reg_number: req.body.reg_number
-            }
+            }, function(err, thePlate) {
 
-            if (!regData || !regData.reg_number) {
-                req.flash('error', 'Plate input should not be blank');
-                res.render('reg_numbers');
-            } else {
-
-                models.Plate.find({
+                if (err) {
+                    return done(err)
+                }
+                models.Plate.create({
                     reg_number: req.body.reg_number
-                }, function(err, thePlate) {
-
+                }, function(err, result) {
                     if (err) {
                         return done(err)
                     }
-                    models.Plate.create({
-                        reg_number: req.body.reg_number
-                    }, function(err, result) {
+
+                    models.Plate.find({}, function(err, result) {
                         if (err) {
                             return done(err)
                         }
+                        console.log(result.reg_number);
 
-                        models.Plate.find({}, function(err, result) {
-                            if (err) {
-                                return done(err)
-                            }
-                            console.log(result.reg_number);
-
-                            display = result;
-                            var data = {
-                                reg_num: display
-                            }
-
-                            res.render('reg_numbers', data);
-                        });
-
-                    })
-                });
-            }
-        };
-
-        const filterData = function(req, res, done) {
-
-            var placeData = {
-                location: req.body.location
-            }
-
-
-            if (!placeData || !placeData.location) {
-                req.flash('error', 'Please select a location');
-                res.render('reg_numbers');
-            } else {
-
-
-                if (placeData.location.startsWith('CA')) {
-                    return placeData;
-
-                }
-                
-                var j = 'CA 99-000';
-
-                models.Plate.find({reg_number: j}, function(err, thePlate) {
-                        if (err) {
-                            return done(err)
-                        }
-
-                        display = thePlate;
+                        display = result;
                         var data = {
                             reg_num: display
                         }
 
                         res.render('reg_numbers', data);
                     });
+
+                })
+            });
+        }
+    };
+
+    const filterData = function(req, res, done) {
+
+        var placeData = {
+            location: req.body.location
+        }
+
+
+        if (!placeData || !placeData.location) {
+            req.flash('error', 'Please select a location');
+            res.render('reg_numbers');
+        } else {
+
+            models.Plate.find({
+
+            }, function(err, thePlate) {
+
+                var loc = '';
+                if (err) {
+                    return done(err)
                 }
-            }
+
+                if (placeData.location == 'capetown') {
+                    loc = 'CA';
+                }
+                else if (placeData.location == 'bellville') {
+                    loc = 'CY';
+                }
+                else if (placeData.location == 'paarl') {
+                    loc = 'CJ';
+                    console.log(loc + ' loc');
+                }
+
+                function king(input) {
+                    return input.reg_number.startsWith(loc);
+                }
 
 
-            return {
-                index,
-                filterData
-            };
-        };
+                var v = thePlate.filter(king);
+
+                display = v;
+
+                var data = {
+                    reg_num: display
+                }
+
+                res.render('reg_numbers', data);
+            });
+        }
+    }
+
+
+    return {
+        index,
+        filterData
+    };
+};
